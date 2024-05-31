1a23ae661523d723a4420ae34b5f03af2cf0c216
Watermel12 <dang.tranbeduahau@hcmut.edu.vn>
2024-05-31 18:22:13 +0700

    update chat

--- src/pages/Chat/Child/MessageContainer.jsx
+++ src/pages/Chat/Child/MessageContainer.jsx
@@ -2,37 +2,32 @@ import React, { useEffect } from "react";
 import {
   Box,
   Grid,
-  Typography,
-  Card,
-  CardContent,
-  CardMedia,
-  CardActionArea,
-  duration,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import Message from "./Message";
 import List from "./List/List";
 import Detail from "./Detail";
 import { useChatStore } from "../../../lib/chatStore";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
 function MessageContainer(prop) {
   const theme = useTheme();
   const isMd = useMediaQuery(theme.breakpoints.up("md"));
-  // console.log(prop.userData)
   const { chatId, ChangeCall } = useChatStore();
-  useEffect(()=>{
-    ChangeCall(prop.userInfo)
-  },[])
+
+  useEffect(() => {
+    ChangeCall(prop.userInfo);
+  }, []);
+
   return (
     <Box
       sx={{
-        // height: "90%",
         marginTop: "30px",
-        backgroundColor: "#A4ADBC",
-        borderRadius: isMd?"12px":"",
+        backgroundColor: "black",
+        borderRadius: isMd ? "12px" : "",
         border: "1px solid rgba(255,255,255,0.125)",
         backdropFilter: "blur(19px) saturate(180%)",
+        padding: "20px",
       }}
     >
       <Box>
@@ -44,9 +39,6 @@ function MessageContainer(prop) {
             <Grid item xs={8}>
               {chatId && <Message userData={prop.userData} />}
             </Grid>
-            {/* <Grid item xs={3}>
-          {chatId&&<Detail />}
-          </Grid> */}
           </Grid>
         ) : (
           <Grid container>
@@ -54,7 +46,7 @@ function MessageContainer(prop) {
               <List userInfo={prop.userInfo} />
             </Grid>}
             {chatId && <Grid item xs={12}>
-              {chatId && <Message userData={prop.userData} userInfo={prop.userInfo}/>}
+              {chatId && <Message userData={prop.userData} userInfo={prop.userInfo} />}
             </Grid>}
           </Grid>
         )}
